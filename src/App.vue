7f86e05d7179a9597d2ee359ce94ac4b886d948d
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-05-29 06:05:09 +0200

    kesz javitva

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,10 @@ export default {
 <template>
   <div>
     <Navbar />
-    <router-view />
+    <div class="main-content-wrapper d-flex justify-content-center align-items-start py-4" style="min-height:80vh;">
+      <div class="main-content bg-white rounded shadow p-4 w-100" style="max-width:1200px;">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
